7e4b6e393812e764a3390abac55d7d423ea4b54e
Marcio <sni.marcio@gmail.com>
2015-11-13 20:42:02 -0200

    closes #2

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Site institucional do Senai - Brusque</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
