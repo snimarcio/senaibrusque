b63fbd4f5dcb603939775130a8333e27910704a1
Marcio <sni.marcio@gmail.com>
2015-11-13 22:01:52 -0200

    closes #3

--- index.php
+++ index.php
@@ -3,11 +3,34 @@
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <title>Site institucional do Senai - Brusque</title>
+        <title>Site Institucional do SENAI - Brusque</title>
+
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/estilo.css"/>
+
+        <!-- javascript -->
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/meuscript.js"></script>
+
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container-fluid">
+            <header class="jumbotron text-center" id="topo">
+                <h1>SENAI/SC Brusque</h1>
+                <p>Ola mundo do bootstrap</p>
+                <form role="form">
+                    <div class="form-inline">
+                        <div class="input-group input-group-sm">
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-envelope"></span>
+                            </span>
+                            <input type="email" class="form-control" id="email" placeholder="nome@dominio.com.br">
+                        </div>
+                    </div>                    
+                    <button type="submit" class="btn btn-success">Cadastrar</button>
+                </form>
+            </header>
+        </div>
     </body>
 </html>
