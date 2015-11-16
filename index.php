0047b23b8add51605ab1d079bce371a0b5dbd54e
Marcio <sezerino@yahoo.com.br>
2015-11-16 21:56:20 -0200

    closes #19

--- index.php
+++ index.php
@@ -11,97 +11,134 @@
         <!-- javascript -->
         <script src="js/jquery-1.11.3.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
-        <script src="js/meuscript.js"></script>
+
 
     </head>
     <body data-spy="scroll" data-target=".navbar" data-offset="50">
-     
-            <header class="jumbotron text-center" id="topo">
-                <h1>SENAI/SC Brusque</h1>
-                <p>Ola mundo do bootstrap</p>
-                <form role="form">
-                    <div class="form-inline">
-                        <div class="input-group input-group-sm">
-                            <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-envelope"></span>
-                            </span>
-                            <input type="email" class="form-control" id="email" placeholder="nome@dominio.com.br">
-                        </div>
-                    </div>                    
-                    <button type="submit" class="btn btn-success">Cadastrar</button>
-                </form>
-            </header>
 
-
-            <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>                        
-                        </button>
-                        <a class="navbar-brand" href="#">SENAI</a>
+        <header class="jumbotron text-center" id="topo">
+            <h1>SENAI/SC Brusque</h1>
+            <p>Ola mundo do bootstrap</p>
+            <form role="form">
+                <div class="form-inline">
+                    <div class="input-group input-group-sm">
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-envelope"></span>
+                        </span>
+                        <input type="email" class="form-control" id="email" placeholder="nome@dominio.com.br">
                     </div>
-                    <div>
-                        <div class="collapse navbar-collapse" id="myNavbar">
-                            <ul class="nav navbar-nav">
-                                <li><a href="#section1">Início</a></li>
-                                <li><a href="#section2">Cursos</a></li>
-                                <li><a href="#section3">Notícias</a></li>
-                                <li><a href="#section4">Fotos</a></li>
-                                <li><a href="#section5">Vídeos</a></li>
-                                <li><a href="#section6">Contato</a></li>
-                                <li><a href="#section7">Localização</a></li>
-                                
-                                <!--
-                                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Seção 4 <span class="caret"></span></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="#section41">Seção 4-1</a></li>
-                                        <li><a href="#section42">Seção 4-2</a></li>
-                                    </ul>
-                                </li>-->
-                            </ul>
-                        </div>
+                </div>                    
+                <button type="submit" class="btn btn-success">Cadastrar</button>
+            </form>
+        </header>
+
+
+        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand" href="#">SENAI</a>
+                </div>
+                <div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#section1">Início</a></li>
+                            <li><a href="#section2">Cursos</a></li>
+                            <li><a href="#section3">Notícias</a></li>
+                            <li><a href="#section4">Fotos</a></li>
+                            <li><a href="#section5">Vídeos</a></li>
+                            <li><a href="#section6">Contato</a></li>
+                            <li><a href="#section7">Localização</a></li>
+
+                            <!--
+                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Seção 4 <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#section41">Seção 4-1</a></li>
+                                    <li><a href="#section42">Seção 4-2</a></li>
+                                </ul>
+                            </li>-->
+                        </ul>
                     </div>
                 </div>
-            </nav>
-            <div id="section1" class="container-fluid">
-                <h1>Início</h1>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             </div>
-            <div id="section2" class="container-fluid">
-                <h1>Cursos</h1>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            </div>
-            <div id="section3" class="container-fluid">
-                <h1>Notícias</h1>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            </div>
-            <div id="section4" class="container-fluid">
-                <h1>Fotos</h1>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            </div>
-            <div id="section5" class="container-fluid">
-                <h1>Vídeos</h1>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            </div>
-            <div id="section6" class="container-fluid">
-                <h1>Contato</h1>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            
-          </div>
-        <div id="section7" class="container-fluid">
-                <h1>Localização</h1>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </nav>
+        <div id="section1" class="container-fluid">
+            <h1>Início</h1>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="img_chania.jpg" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="http://www.snap-undp.org/WeeklyHighlights/Lists/Photos/placeholder.png" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="http://www.snap-undp.org/WeeklyHighlights/Lists/Photos/placeholder.png" alt="Flower">
+                    </div>
+
+                    <div class="item">
+                        <img src="http://www.snap-undp.org/WeeklyHighlights/Lists/Photos/placeholder.png" alt="Flower">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </div>
-    </body>
+        <div id="section2" class="container-fluid">
+            <h1>Cursos</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <div id="section3" class="container-fluid">
+            <h1>Notícias</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <div id="section4" class="container-fluid">
+            <h1>Fotos</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <div id="section5" class="container-fluid">
+            <h1>Vídeos</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <div id="section6" class="container-fluid">
+            <h1>Contato</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+
+        </div>
+        <div id="section7" class="container-fluid">
+            <h1>Localização</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+    </div>
+    <script src="js/meuscript.js"></script>
+</body>
 </html>
