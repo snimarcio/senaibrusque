f7786a1f9f196a5140b4f5d6fa76c5226d5d9bfb
Marcio <sezerino@yahoo.com.br>
2015-11-16 21:09:17 -0200

    closes #8 closes #9

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
         <script src="js/meuscript.js"></script>
 
     </head>
-    <body>
-        <div class="container-fluid">
+    <body data-spy="scroll" data-target=".navbar" data-offset="50">
+     
             <header class="jumbotron text-center" id="topo">
                 <h1>SENAI/SC Brusque</h1>
                 <p>Ola mundo do bootstrap</p>
@@ -31,6 +31,77 @@
                     <button type="submit" class="btn btn-success">Cadastrar</button>
                 </form>
             </header>
+
+
+            <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>                        
+                        </button>
+                        <a class="navbar-brand" href="#">SENAI</a>
+                    </div>
+                    <div>
+                        <div class="collapse navbar-collapse" id="myNavbar">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#section1">Início</a></li>
+                                <li><a href="#section2">Cursos</a></li>
+                                <li><a href="#section3">Notícias</a></li>
+                                <li><a href="#section4">Fotos</a></li>
+                                <li><a href="#section5">Vídeos</a></li>
+                                <li><a href="#section6">Contato</a></li>
+                                <li><a href="#section7">Localização</a></li>
+                                
+                                <!--
+                                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Seção 4 <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#section41">Seção 4-1</a></li>
+                                        <li><a href="#section42">Seção 4-2</a></li>
+                                    </ul>
+                                </li>-->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div id="section1" class="container-fluid">
+                <h1>Início</h1>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            </div>
+            <div id="section2" class="container-fluid">
+                <h1>Cursos</h1>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            </div>
+            <div id="section3" class="container-fluid">
+                <h1>Notícias</h1>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            </div>
+            <div id="section4" class="container-fluid">
+                <h1>Fotos</h1>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            </div>
+            <div id="section5" class="container-fluid">
+                <h1>Vídeos</h1>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            </div>
+            <div id="section6" class="container-fluid">
+                <h1>Contato</h1>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            
+          </div>
+        <div id="section7" class="container-fluid">
+                <h1>Localização</h1>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+                <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            </div>
         </div>
     </body>
 </html>
