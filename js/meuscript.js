5e4ec678035b89616d693a6f0eba5d91e2ee7ca2
Marcio <sezerino@yahoo.com.br>
2015-11-16 21:25:16 -0200

    closes #9

--- js/meuscript.js
+++ js/meuscript.js
@@ -3,5 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+// Add scrollspy to <body>
+$('body').scrollspy({target: ".navbar", offset: 50}); 
 
+// Add smooth scrolling to all links inside a navbar
+$("#myNavbar a").on('click', function(event){
+
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash (#)
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+});
 
